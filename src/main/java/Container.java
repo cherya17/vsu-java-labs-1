b00dd6e71c768759d434f66b58fdfe394eaf49d4
Alexander <cheremiskinalexander@yandex.ru>
2021-02-21 11:36:59 +0300

    fix constructor + create add-method

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+
 public class Container {
+
+    // TODO: добавить константу для изменения capacity
 
     private int data[];
     private int size;
@@ -30,9 +34,50 @@ public class Container {
      * @param data элементы для инициализации контейнера
      */
     public Container(int[] data) {
+        capacity = Math.max(data.length + 10, MinCapacity);
         size = data.length;
-        this.data = new int[size];
-        System.arraycopy(this.data, 0, data, 0, size);
+        this.data = new int[capacity];
+        copy(data, this.data);
+    }
+
+    /**
+     * Добавление элемента в контейнер
+     * @param num число для добавления в контейнер
+     */
+    public void add(int num) {
+        if (size + 1 >= capacity) {
+            increaseContainer(capacity + 10);
+        }
+        data[size++] = num;
+    }
+
+    /**
+     * Изменение максимальной вместимости контейнера
+     * @param newCapacity новая вместимость контейнера
+     */
+    private void increaseContainer (int newCapacity) {
+        capacity = newCapacity;
+        int[] newData = new int[capacity];
+        if (size > 0) {
+            copy(data, newData);
+        }
+        data = newData;
+    }
+
+
+    /**
+     * Копирование одного массива в другой
+     * @param source данные для копирования
+     * @param data куда копируются данные
+     */
+    private void copy (int [] source, int [] data) {
+        if (source.length > data.length) {
+            data = new int [source.length];
+        }
+
+        for (int i = 0; i < source.length; i++) {
+            data[i] = source[i];
+        }
     }
 
     /**
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Container C = new Container(new int[]{1, 2, 4, 5});
+        C.add(33);
+        C.add(35);
+        for (int i = 0; i < C.getSize(); i++) {
+            System.out.println(C.getData()[i]);
+        }
 
     }
 }
