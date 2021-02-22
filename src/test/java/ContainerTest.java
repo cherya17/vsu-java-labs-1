c1ccf7a4e111ce8151cd193a2e135a1427c666ac
Alexander <cheremiskinalexander@yandex.ru>
2021-02-22 10:50:11 +0300

    add test for 'insert' method

--- src/test/java/ContainerTest.java
+++ src/test/java/ContainerTest.java
@@ -26,7 +26,7 @@ class ContainerTest {
     }
 
     @Test
-    void push() {
+    void pushTest() {
         container.push(1);
         assertEquals(size + 1, container.getSize());
 
@@ -38,7 +38,39 @@ class ContainerTest {
     }
 
     @Test
-    void remove() {
+    void insertTest() {
+
+        // start of container
+        insertTestStep(111, 0);
+
+        // middle of container
+        insertTestStep(222, 2);
+
+        // end of container
+        insertTestStep(333, size - 1);
+        insertTestStep(444, size);
+    }
+
+    void insertTestStep(int num, int index) {
+        container.insert(num, index);
+        assertEquals(size + 1, container.getSize());
+
+        assertEquals(num, container.getItem(index));
+
+        for (int i = 0; i < index; i++) {
+            assertEquals(data[i], container.getItem(i));
+        }
+
+        for (int i = index + 1; i < size + 1; i++) {
+            assertEquals(data[i - 1], container.getItem(i));
+        }
+
+        container.copy(container.getData(), data);
+        size = container.getSize();
+    }
+
+    @Test
+    void removeTest() {
         int index = 1;
         container.remove(index);
 
@@ -52,27 +84,19 @@ class ContainerTest {
     }
 
     @Test
-    void getItem() {
+    void getItemTest() {
         assertEquals(1, container.getItem(0));
         assertEquals(2, container.getItem(1));
         assertEquals(3, container.getItem(2));
     }
 
     @Test
-    void copy() {
+    void copyTest() {
         Container newContainer = new Container(container.getCapacity());
         container.copy(data, newContainer.getData());
 
         for (int i = 0; i < size; i++) {
             assertEquals(data[i], newContainer.getItem(i));
         }
-    }
-
-    @Test
-    void getSize() {
-    }
-
-    @Test
-    void getData() {
     }
 }
